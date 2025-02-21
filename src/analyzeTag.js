b82e26fb9eb4f138cfa9e6112b7372afe15fa1f9
Pierre-Louis Serré <pierre-louisserre@MacBook-Air-de-Pierre-Louis.local>
2025-02-21 20:01:06 +0100

    finish analyzeTag

--- src/analyzeTag.js
+++ src/analyzeTag.js
@@ -1,95 +1,121 @@
-let allHtml;
-let openTag;
-let closeTag; 
-let content;
-let tag;
-let cssClass;
-let analyze;
-
 //Maybe in the futur I will do not need some of properties in the final object
-//TODO réécrire en mode class et vérifier que l'init se passe bien
-
-export function analyzeTag(html){
-   initAllVariables();
-    allHtml = html;
-    determinateOpenTag();
-    determinateTag();
-    determinateCloseTag();
-    determinateContent();
-    determinateCssClass();
-    constructAnalyze();
-    return analyze;
-};
-
-const initAllVariables = ()=>{
-    allHtml = '';
-    openTag = '';
-    closeTag = '';
-    content = '';
-    tag = '';
-    cssClass = '';
-    analyze = {};
-}
-
-const determinateOpenTag = ()=>{
-    for(let i = 0; i < allHtml.length; i ++){
-        const content = allHtml[i];
-        if(content == '>' && openTag == ''){
-            openTag = allHtml.substring(0, i)+content;
-        }
+export default class AnalyzeTag{
+    constructor(){
+        this.allHtml = '';
+        this.openTag = '';
+        this.closeTag = '';
+        this.content = '';
+        this.tag = '';
+        this.cssClass = '';
+        this.cssId = '';
+        this.contentOnlyText = true;
+        this.analyze = {};
     }
-};
 
-const determinateTag = ()=>{
-    const isComplexTag = openTag.includes('<');
-    if(isComplexTag){
-            const startTag = openTag.split(' ')[0];
-            tag = startTag.replace('<','').replace('>', '');
-        }
-    else
-        tag = openTag.replace('<','').replace('>', '');
-}
-
-const determinateCloseTag = ()=>{
-    closeTag = '</'+tag+'>';
-}
-
-const determinateContent = ()=>{
-    content = allHtml.replace(openTag,'').replace(closeTag,'');
-}
-
-//TODO improve this code
-//faut que je finisse la fin de récupération de la class
-const determinateCssClass = ()=>{
-    cssClass = '';
-    const classAttribut = 'class="';
-    let recordingCssClass = false;
-    let indexRecording = 0;
-    for(let i = 0; i < openTag.length; i ++){
-        const content = openTag[i];
-            if(!recordingCssClass){
-            const classAttributCandidate = openTag.substring(i, i+classAttribut.length);
-            if(classAttributCandidate===classAttribut){
-                recordingCssClass = true;
-                indexRecording = i + classAttribut.length;
+    analyzeData = (html)=>{
+        this.allHtml = html;
+        this.determinateOpenTag();
+        this.determinateTag();
+        this.determinateCloseTag();
+        this.determinateContent();
+        this.determinateCssClass();
+        this.determinateIdCss();
+        this.constructAnalyze();
+        return this.analyze;
+    }
+    
+    determinateOpenTag = ()=>{
+        for(let i = 0; i < this.allHtml.length; i ++){
+            const content = this.allHtml[i];
+            if(content == '>' && this.openTag == ''){
+                this.openTag = this.allHtml.substring(0, i)+content;
             }
         }
-        else if (content == '"'){
-            continue;
-        }
-        else if (content == '>'){
-            break;
-        }
-        else if (i >= indexRecording){
-            cssClass += content;
+    };
+    
+    determinateTag = ()=>{
+        const isComplexTag = this.openTag.includes('<');
+        if(isComplexTag){
+                const startTag = this.openTag.split(' ')[0];
+                this.tag = startTag.replace('<','').replace('>', '');
+            }
+        else
+            this.tag = this.openTag.replace('<','').replace('>', '');
+    }
+    
+    determinateCloseTag = ()=>{
+        this.closeTag = '</'+this.tag+'>';
+    }
+    
+    determinateContent = ()=>{
+        this.content = this.allHtml.replace(this.openTag,'').replace(this.closeTag,'');
+        if(this.content.includes('<'))
+            this.contentOnlyText = false;
+    }
+    
+    //TODO improve this code
+    determinateCssClass = ()=>{
+        this.cssClass = '';
+        const classAttribut = 'class="';
+        let recordingCssClass = false;
+        let indexRecording = 0;
+        let quoteNumber = 0;
+        for(let i = 0; i < this.openTag.length; i ++){
+            const content = this.openTag[i];
+            if(!recordingCssClass){
+                const classAttributCandidate = this.openTag.substring(i, i+classAttribut.length);
+                if(classAttributCandidate===classAttribut){
+                    recordingCssClass = true;
+                    indexRecording = i + classAttribut.length;
+                }
+            }
+            else if (content == '"' && quoteNumber == 1){
+                break;
+            }
+            else if (content == '"'){
+                quoteNumber +=1;
+                continue;
+            }
+            else if (i >= indexRecording){
+                this.cssClass += content;
+            }
         }
     }
-}
 
-const constructAnalyze=()=>{
-    analyze = {
-        tagName : tag,
-        content : content,
-        cssClass : cssClass
-    };
+    //TODO improve this code
+    determinateIdCss = () => {
+        this.cssId = '';
+        const classAttribut = 'id="';
+        let recordingCssId = false;
+        let indexRecording = 0;
+        for(let i = 0; i < this.openTag.length; i ++){
+            const content = this.openTag[i];
+                if(!recordingCssId){
+                const classAttributCandidate = this.openTag.substring(i, i+classAttribut.length);
+                if(classAttributCandidate===classAttribut){
+                    recordingCssId = true;
+                    indexRecording = i + classAttribut.length;
+                }
+            }
+            else if (content == '"'){
+                continue;
+            }
+            else if (content == '>' || content == ' '){
+                break;
+            }
+            else if (i >= indexRecording){
+                this.cssId += content;
+            }
+        }
+    }
+    
+    constructAnalyze=()=>{
+        this.analyze = {
+            tagName : this.tag,
+            content : this.content,
+            cssClass : this.cssClass,
+            cssId : this.cssId,
+            contentOnlyText : this.contentOnlyText
+        };
+    }
 }
--- tests/analyzeTag.test.js
+++ tests/analyzeTag.test.js
@@ -1,50 +1,64 @@
-import { analyzeTag } from "../src/analyzeTag";
-import { jest } from '@jest/globals';
-
-
+import AnalyzeTag from '../src/analyzeTag';
 
 test('Analyze simple Tag without anything', ()=>{
     const html = '<title>Liste des Pokémon de la première génération — Poképédia</title>';
-    const result = analyzeTag(html);
+    const analyzeTag = new AnalyzeTag();
+    const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : "title", content: "Liste des Pokémon de la première génération — Poképédia", cssClass:''};
-    expect(compareTwoAnalyzeTag(expected, result, "first")).toBe(true);
+    const expected = {tagName : "title", content: "Liste des Pokémon de la première génération — Poképédia", cssClass:'', cssId : '', "contentOnlyText":true};
+    expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
 
-//1 class sans texte
 test('Analyze simple Tag with only one class css', async ()=>{
     const html = '<ul class="vector-menu-content-list"></ul>';
-    const result = analyzeTag(html);
+    const analyzeTag = new AnalyzeTag();
+    const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : 'ul', content:'', cssClass : 'vector-menu-content-list'};
-    expect(compareTwoAnalyzeTag(expected, result, "second")).toBe(true);
+    const expected = {tagName : 'ul', content:'', cssClass : 'vector-menu-content-list', cssId : '', contentOnlyText : true};
+    expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
 
-//1 class avec texte 
+test('Analyze simple Tag with one class css and texte', async ()=>{
+    const html = '<span class="vector-dropdown-label-text">Outils personnels</span>';
+    const analyzeTag = new AnalyzeTag();
+    const result = analyzeTag.analyzeData(html);
 
-//1 sous div 
+    const expected = {tagName : 'span', content:'Outils personnels', cssClass : 'vector-dropdown-label-text', cssId : '', contentOnlyText : true};
+    expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
+});
 
-//1 attribut 1 class
+test('Analyze Tags with two level, one class css and texte', async ()=>{
+    const html = '<h1 class="firstHeading mw-first-heading"><span class="mw-page-title-main">Liste des Pokémon de la première génération</span></h1>';
+    const analyzeTag = new AnalyzeTag();
+    const result = analyzeTag.analyzeData(html);
 
-//1 attribut 1 class 1 sous div
+    const expected = {tagName : 'h1', content:'<span class="mw-page-title-main">Liste des Pokémon de la première génération</span>', cssClass : 'firstHeading mw-first-heading', cssId : '', contentOnlyText : false};
+    expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
+});
 
-//n attributs n class
+test('Analyze Tags with one attribut, one class css and texte', async ()=>{
+    const html = '<div id="vector-main-menu-pinned-container" class="vector-pinned-container">déplacer vers la barre latérale</div>';
+    const analyzeTag = new AnalyzeTag();
+    const result = analyzeTag.analyzeData(html);
 
-//n attributs n class n sous div 
+    const expected = {tagName : 'div', content:'déplacer vers la barre latérale', cssClass : 'vector-pinned-container', cssId:'vector-main-menu-pinned-container',  contentOnlyText : true};
+    expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
+});
 
-const compareTwoAnalyzeTag = (expectedTag, resultTag) => {
-    console.log(JSON.stringify(expectedTag));
-    console.log(JSON.stringify(resultTag));
-    if(expectedTag.tagName === resultTag.tagName && expectedTag.content === resultTag.content
-            && expectedTag.cssClass === resultTag.cssClass)
-            {
-                
-                console.log("true");
-                return true;
-        }
-    else{
-        
-            console.log("false");
-            return false;
-        }
-}
+test('Analyze Tags with one attribut, one class css and texte (inverse last one)', async ()=>{
+    const html = '<div class="vector-pinned-container" id="vector-main-menu-pinned-container">déplacer vers la barre latérale</div>';
+    const analyzeTag = new AnalyzeTag();
+    const result = analyzeTag.analyzeData(html);
+
+    const expected = {tagName : 'div', content:'déplacer vers la barre latérale', cssClass : 'vector-pinned-container', cssId:'vector-main-menu-pinned-container',  contentOnlyText : true};
+    expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
+});
+
+test('Analyze Tags with one attribut, two class css, and many sub element with texte', async ()=>{
+    const html = '<li id="ca-view" class="selected vector-tab-noicon mw-list-item"><a href="/Liste_des_Pok%C3%A9mon_de_la_premi%C3%A8re_g%C3%A9n%C3%A9ration"><span>Lire</span></a></li>';
+    const analyzeTag = new AnalyzeTag();
+    const result = analyzeTag.analyzeData(html);
+
+    const expected = {tagName : 'li', content:'<a href="/Liste_des_Pok%C3%A9mon_de_la_premi%C3%A8re_g%C3%A9n%C3%A9ration"><span>Lire</span></a>', cssClass : 'selected vector-tab-noicon mw-list-item', cssId:'ca-view',  contentOnlyText : false};
+    expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
+});
