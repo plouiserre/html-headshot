cc7d0b7dec469e365073639b3caf136e245701e4
Pierre-Louis Serré <pierre-louisserre@MacBook-Air-de-Pierre-Louis.local>
2025-02-21 20:38:42 +0100

    replace function extractTag by a class

--- src/extractTag.js
+++ src/extractTag.js
@@ -1,43 +1,89 @@
-let isSimpleTag = false;
-
-export function extractTag(tag, html){
-    let contentTag = '';
-    let sameOpenTag = 0;
-    determinateIsSimpleTag(html);
-    const closedTag = generateClosedTag(tag);
-    const openTag = generateOpenTag(tag);
-    for(let i = 0; i < html.length; i ++){
-        const openTagCandidate = generateTagCandidate(i, openTag.length, html);
-        const closedTagCandidate = generateTagCandidate(i, closedTag.length, html);
-        if(openTagCandidate == openTag){
-            sameOpenTag +=1 ;
-        }
-        if(closedTagCandidate == closedTag && sameOpenTag <= 0)
-            break;
-        if(closedTagCandidate == closedTag)
-            sameOpenTag -= 1;
-        contentTag+=html[i];
+export default class ExtractTag{
+    constructor(){
+        this.isSimpleTag = false;
     }
-    const tagBuild = openTag+contentTag+closedTag;
-    return tagBuild;
+
+    extract = (tag, html) =>{
+        let contentTag = '';
+        let sameOpenTag = 0;
+        this.determinateIsSimpleTag(html);
+        const closedTag = this.generateClosedTag(tag);
+        const openTag = this.generateOpenTag(tag);
+        for(let i = 0; i < html.length; i ++){
+            const openTagCandidate = this.generateTagCandidate(i, openTag.length, html);
+            const closedTagCandidate = this.generateTagCandidate(i, closedTag.length, html);
+            if(openTagCandidate == openTag){
+                sameOpenTag +=1 ;
+            }
+            if(closedTagCandidate == closedTag && sameOpenTag <= 0)
+                break;
+            if(closedTagCandidate == closedTag)
+                sameOpenTag -= 1;
+            contentTag+=html[i];
+        }
+        const tagBuild = openTag+contentTag+closedTag;
+        return tagBuild;
+    }
+
+    generateOpenTag = (tag)=>{
+        const openTag = this.isSimpleTag ? '<'+tag+'>' : '<'+tag;+' ';
+        return openTag;
+    }
+    
+    determinateIsSimpleTag = (html)=>{
+        return this.isSimpleTag = html[0] != ' ' ? true : false;
+    }
+    
+    generateTagCandidate = (index, closedTagLength, html)=>{
+        const endSubstring = index + closedTagLength;
+        const closedTagCandidate = html.substring(index , endSubstring);
+        return closedTagCandidate;
+    }
+    
+    generateClosedTag = (tag)=>{
+        let closedTag = '</'+tag+'>';
+        return closedTag;
+    };
 }
 
-const generateOpenTag = (tag)=>{
-    const openTag = isSimpleTag ? '<'+tag+'>' : '<'+tag;+' ';
-    return openTag;
-}
+// export function extractTag(tag, html){
+//     let contentTag = '';
+//     let sameOpenTag = 0;
+//     determinateIsSimpleTag(html);
+//     const closedTag = generateClosedTag(tag);
+//     const openTag = generateOpenTag(tag);
+//     for(let i = 0; i < html.length; i ++){
+//         const openTagCandidate = generateTagCandidate(i, openTag.length, html);
+//         const closedTagCandidate = generateTagCandidate(i, closedTag.length, html);
+//         if(openTagCandidate == openTag){
+//             sameOpenTag +=1 ;
+//         }
+//         if(closedTagCandidate == closedTag && sameOpenTag <= 0)
+//             break;
+//         if(closedTagCandidate == closedTag)
+//             sameOpenTag -= 1;
+//         contentTag+=html[i];
+//     }
+//     const tagBuild = openTag+contentTag+closedTag;
+//     return tagBuild;
+// }
 
-const determinateIsSimpleTag = (html)=>{
-    return isSimpleTag = html[0] != ' ' ? true : false;
-}
+// const generateOpenTag = (tag)=>{
+//     const openTag = isSimpleTag ? '<'+tag+'>' : '<'+tag;+' ';
+//     return openTag;
+// }
 
-const generateTagCandidate = (index, closedTagLength, html)=>{
-    const endSubstring = index + closedTagLength;
-    const closedTagCandidate = html.substring(index , endSubstring);
-    return closedTagCandidate;
-}
+// const determinateIsSimpleTag = (html)=>{
+//     return isSimpleTag = html[0] != ' ' ? true : false;
+// }
 
-const generateClosedTag = (tag)=>{
-    let closedTag = '</'+tag+'>';
-    return closedTag;
-};
+// const generateTagCandidate = (index, closedTagLength, html)=>{
+//     const endSubstring = index + closedTagLength;
+//     const closedTagCandidate = html.substring(index , endSubstring);
+//     return closedTagCandidate;
+// }
+
+// const generateClosedTag = (tag)=>{
+//     let closedTag = '</'+tag+'>';
+//     return closedTag;
+// };
--- tests/extractTag.test.js
+++ tests/extractTag.test.js
@@ -1,19 +1,21 @@
-import { extractTag } from "../src/extractTag";
-
+import ExtractTag from "../src/extractTag";
 
 test('can analyze simple Tag', ()=>{
     const tag = 'title';
     const html = 'Liste des Pokémon de la première génération — Poképédia</title><script>(function(){var className="client-js vector-feature-language-in-header';
-    const result = extractTag(tag, html);
+    const extractTag = new ExtractTag();
+    const result = extractTag.extract(tag, html);
 
     const resultWaiting = '<title>Liste des Pokémon de la première génération — Poképédia</title>'
     expect(resultWaiting).toBe(result);
 });
 
+
 test('can analyze hard Tag', ()=>{
     const tag = 'span';
     const html = ' class="vector-dropdown-label-text">Menu principal</span> </label>';
-    const result = extractTag(tag, html);
+    const extractTag = new ExtractTag();
+    const result = extractTag.extract(tag, html);
 
     const resultWaiting = '<span class="vector-dropdown-label-text">Menu principal</span>'
     expect(resultWaiting).toBe(result);
@@ -23,7 +25,8 @@ test('can analyze hard Tag', ()=>{
 test('can analyze many Tags', ()=>{
     const tag = 'header';
     const html = ' class="mw-body-header vector-page-titlebar"><h1 id="firstHeading" class="firstHeading mw-first-heading"><span class="mw-page-title-main">Liste des Pokémon de la première génération</span></h1></header> <div class="vector-page-toolbar"> <div class="vector-page';
-    const result = extractTag(tag, html);
+    const extractTag = new ExtractTag();
+    const result = extractTag.extract(tag, html);
 
     const resultWaiting = '<header class="mw-body-header vector-page-titlebar"><h1 id="firstHeading" class="firstHeading mw-first-heading"><span class="mw-page-title-main">Liste des Pokémon de la première génération</span></h1></header>'
     expect(resultWaiting).toBe(result);
@@ -33,7 +36,8 @@ test('can analyze many Tags', ()=>{
 test('can analyze many same tags and different', ()=>{
     const tag = 'div';
     const html = ' id="p-associated-pages" class="vector-menu vector-menu-tabs mw-portlet mw-portlet-associated-pages"><div class="vector-menu-content"><ul class="vector-menu-content-list"><li id="ca-nstab-main" class="selected vector-tab-noicon mw-list-item"><a href="/Liste_des_Pok%C3%A9mon_de_la_premi%C3%A8re_g%C3%A9n%C3%A9ration" title="Voir le contenu de la page [c]" accesskey="c"><span>Page</span></a></li><li id="ca-talk" class="new vector-tab-noicon mw-list-item"><a href="/index.php?title=Discussion:Liste_des_Pok%C3%A9mon_de_la_premi%C3%A8re_g%C3%A9n%C3%A9ration&amp;action=edit&amp;redlink=1" rel="discussion" title="Discussion au sujet de cette page de contenu (page inexistante) [t]" accesskey="t"><span>Discussion</span></a></li></ul></div></div><div id="p-variants" class="vector-dropdown emptyPortlet"><input type="checkbox" ';
-    const result = extractTag(tag, html);
+    const extractTag = new ExtractTag();
+    const result = extractTag.extract(tag, html);
 
     const resultWaiting = '<div id="p-associated-pages" class="vector-menu vector-menu-tabs mw-portlet mw-portlet-associated-pages"><div class="vector-menu-content"><ul class="vector-menu-content-list"><li id="ca-nstab-main" class="selected vector-tab-noicon mw-list-item"><a href="/Liste_des_Pok%C3%A9mon_de_la_premi%C3%A8re_g%C3%A9n%C3%A9ration" title="Voir le contenu de la page [c]" accesskey="c"><span>Page</span></a></li><li id="ca-talk" class="new vector-tab-noicon mw-list-item"><a href="/index.php?title=Discussion:Liste_des_Pok%C3%A9mon_de_la_premi%C3%A8re_g%C3%A9n%C3%A9ration&amp;action=edit&amp;redlink=1" rel="discussion" title="Discussion au sujet de cette page de contenu (page inexistante) [t]" accesskey="t"><span>Discussion</span></a></li></ul></div></div>'
     expect(resultWaiting).toBe(result);
@@ -43,7 +47,8 @@ test('can analyze many same tags and different', ()=>{
 test('can analyze many same tags and different with many levels', ()=>{
     const tag = 'div';
     const html = ' id="p-variants" class="vector-dropdown emptyPortlet"><input type="checkbox" id="p-variants-checkbox" role="button" aria-haspopup="true" data-event-name="ui.dropdown-p-variants" class="vector-dropdown-checkbox" aria-label="Modifier la variante de langue"><label id="p-variants-label" for="p-variants-checkbox" class="vector-dropdown-label cdx-button cdx-button--fake-button cdx-button--fake-button--enabled cdx-button--weight-quiet" aria-hidden="true"><span class="vector-dropdown-label-text">français</span></label><div class="vector-dropdown-content"><div id="p-variants" class="vector-menu mw-portlet mw-portlet-variants emptyPortlet"><div class="vector-menu-content"><ul class="vector-menu-content-list"></ul></div></div></div></div><div id="p-lang-btn" class="vector-dropdown mw-portlet mw-portlet-lang"';
-    const result = extractTag(tag, html);
+    const extractTag = new ExtractTag();
+    const result = extractTag.extract(tag, html);
 
     const resultWaiting = '<div id="p-variants" class="vector-dropdown emptyPortlet"><input type="checkbox" id="p-variants-checkbox" role="button" aria-haspopup="true" data-event-name="ui.dropdown-p-variants" class="vector-dropdown-checkbox" aria-label="Modifier la variante de langue"><label id="p-variants-label" for="p-variants-checkbox" class="vector-dropdown-label cdx-button cdx-button--fake-button cdx-button--fake-button--enabled cdx-button--weight-quiet" aria-hidden="true"><span class="vector-dropdown-label-text">français</span></label><div class="vector-dropdown-content"><div id="p-variants" class="vector-menu mw-portlet mw-portlet-variants emptyPortlet"><div class="vector-menu-content"><ul class="vector-menu-content-list"></ul></div></div></div></div>'
     expect(resultWaiting).toBe(result);
