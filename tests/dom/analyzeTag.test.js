f4b9cd36b3adf049667d689651d32315ef063e78
Pierre-Louis Serré <pierre-louisserre@MacBook-Air-de-Pierre-Louis.local>
2025-04-10 14:28:34 +0200

    have all tag stock in domresult

--- tests/dom/analyzeTag.test.js
+++ tests/dom/analyzeTag.test.js
@@ -5,7 +5,7 @@ test('Analyze simple Tag without anything', ()=>{
     const analyzeTag = new AnalyzeTag();
     const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : "title", content: "Liste des Pokémon de la première génération — Poképédia", cssClass:'', cssId : '', "contentOnlyText":true};
+    const expected = {tagName : "title", completeTag : html, content: "Liste des Pokémon de la première génération — Poképédia", cssClass:'', cssId : '', "contentOnlyText":true};
     expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
 
@@ -14,7 +14,7 @@ test('Analyze simple Tag with only one class css', async ()=>{
     const analyzeTag = new AnalyzeTag();
     const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : 'ul', content:'', cssClass : 'vector-menu-content-list', cssId : '', contentOnlyText : true};
+    const expected = {tagName : 'ul', completeTag: html, content:'', cssClass : 'vector-menu-content-list', cssId : '', contentOnlyText : true};
     expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
 
@@ -23,7 +23,7 @@ test('Analyze simple Tag with one class css and texte', async ()=>{
     const analyzeTag = new AnalyzeTag();
     const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : 'span', content:'Outils personnels', cssClass : 'vector-dropdown-label-text', cssId : '', contentOnlyText : true};
+    const expected = {tagName : 'span', completeTag : html , content:'Outils personnels', cssClass : 'vector-dropdown-label-text', cssId : '', contentOnlyText : true};
     expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
 
@@ -32,7 +32,7 @@ test('Analyze Tags with two level, one class css and texte', async ()=>{
     const analyzeTag = new AnalyzeTag();
     const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : 'h1', content:'<span class="mw-page-title-main">Liste des Pokémon de la première génération</span>', cssClass : 'firstHeading mw-first-heading', cssId : '', contentOnlyText : false};
+    const expected = {tagName : 'h1', completeTag : html ,content:'<span class="mw-page-title-main">Liste des Pokémon de la première génération</span>', cssClass : 'firstHeading mw-first-heading', cssId : '', contentOnlyText : false};
     expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
 
@@ -41,7 +41,7 @@ test('Analyze Tags with one attribut, one class css and texte', async ()=>{
     const analyzeTag = new AnalyzeTag();
     const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : 'div', content:'déplacer vers la barre latérale', cssClass : 'vector-pinned-container', cssId:'vector-main-menu-pinned-container',  contentOnlyText : true};
+    const expected = {tagName : 'div', completeTag : html, content:'déplacer vers la barre latérale', cssClass : 'vector-pinned-container', cssId:'vector-main-menu-pinned-container',  contentOnlyText : true};
     expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
 
@@ -50,7 +50,7 @@ test('Analyze Tags with one attribut, one class css and texte (inverse last one)
     const analyzeTag = new AnalyzeTag();
     const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : 'div', content:'déplacer vers la barre latérale', cssClass : 'vector-pinned-container', cssId:'vector-main-menu-pinned-container',  contentOnlyText : true};
+    const expected = {tagName : 'div', completeTag : html, content:'déplacer vers la barre latérale', cssClass : 'vector-pinned-container', cssId:'vector-main-menu-pinned-container',  contentOnlyText : true};
     expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
 
@@ -59,7 +59,7 @@ test('Analyze Tags with one attribut, two class css, and many sub element with t
     const analyzeTag = new AnalyzeTag();
     const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : 'li', content:'<a href="/Liste_des_Pok%C3%A9mon_de_la_premi%C3%A8re_g%C3%A9n%C3%A9ration"><span>Lire</span></a>', cssClass : 'selected vector-tab-noicon mw-list-item', cssId:'ca-view',  contentOnlyText : false};
+    const expected = {tagName : 'li', completeTag : html, content:'<a href="/Liste_des_Pok%C3%A9mon_de_la_premi%C3%A8re_g%C3%A9n%C3%A9ration"><span>Lire</span></a>', cssClass : 'selected vector-tab-noicon mw-list-item', cssId:'ca-view',  contentOnlyText : false};
     expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
 
@@ -68,7 +68,7 @@ test('Analyze tag with one space before the opens', async()=>{
     const analyzeTag = new AnalyzeTag();
     const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : 'div', content:'<ul></ul>', cssClass : 'vector-settings', cssId:'p-dock-bottom',  contentOnlyText : false};
+    const expected = {tagName : 'div', completeTag :'<div class="vector-settings" id="p-dock-bottom"><ul></ul></div>', content:'<ul></ul>', cssClass : 'vector-settings', cssId:'p-dock-bottom',  contentOnlyText : false};
     expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
 
@@ -78,7 +78,7 @@ test('Analyze tag with many levels with same closedTags', async()=>{
     const analyzeTag = new AnalyzeTag();
     const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : 'div', content:contentHtml, cssClass : 'vector-column-start', cssId:'',  contentOnlyText : false};
+    const expected = {tagName : 'div', completeTag : html, content:contentHtml, cssClass : 'vector-column-start', cssId:'',  contentOnlyText : false};
     expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
 
@@ -87,6 +87,6 @@ test('Analyze tag when in content you have tag inside text', async()=>{
     const analyzeTag = new AnalyzeTag();
     const result = analyzeTag.analyzeData(html);
 
-    const expected = {tagName : 'li', content:'Le contenu est disponible sous licence <a href="/Pok%C3%A9p%C3%A9dia:Copyrights" title="Poképédia:Copyrights">Paternité-Pas d\'Utilisation Commerciale-Partage des Conditions Initiales à l\'Identique 3.0</a> sauf mention contraire.', cssClass : '', cssId:'footer-info-copyright',  contentOnlyText : true};
+    const expected = {tagName : 'li', completeTag : html, content:'Le contenu est disponible sous licence <a href="/Pok%C3%A9p%C3%A9dia:Copyrights" title="Poképédia:Copyrights">Paternité-Pas d\'Utilisation Commerciale-Partage des Conditions Initiales à l\'Identique 3.0</a> sauf mention contraire.', cssClass : '', cssId:'footer-info-copyright',  contentOnlyText : true};
     expect(JSON.stringify(expected)).toBe(JSON.stringify(result));
 });
