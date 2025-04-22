d2a36175fc5de47733fca1ae272a6224030d8d09
Pierre-Louis Serré <pierre-louisserre@MacBook-Air-de-Pierre-Louis.local>
2025-04-22 20:20:38 +0200

    rework identify request to identify text requests

--- tests/request/identitfyRequest.test.js
+++ tests/request/identitfyRequest.test.js
@@ -1,20 +1,41 @@
-import IdentifyRequest from "../../src/request/identifyRequest";
-import { CompareTagArrays, CompareTagsDictionnary } from "../utils/compare";
+import IdentifyRequest from "../../src/search/identifyRequest";
+import {  CompareTagObject } from "../utils/compare";
 
-test('identify the request writing clear', ()=>{
-    const request = '.news > td > .headlines > span > li';
+test('identify the request writing clear with only tag', ()=>{
+    const request = {path :'.news > td > .headlines > span > li', mode : 'tag'};
     const identifyRequest = new IdentifyRequest(request);
 
     const result = identifyRequest.analyze();
 
-    const expected ={};
-    expected["news"] = "class";
-    expected["td"] = "tagName";
-    expected["headlines"] = "class";
-    expected["span"] = "tagName";
-    expected["li"] = "tagName";
+    let expected = constructExpectedResult("tag");
 
-    expect(CompareTagsDictionnary(expected, result)).toBe(true);
+
+    expect(CompareTagObject(expected, result)).toBe(true);
 })
+
+test('identify the request writing clear with only text', ()=>{
+    const request = {path : '.news > td > .headlines > span > li', mode : 'text'};
+    const identifyRequest = new IdentifyRequest(request);
+
+    const result = identifyRequest.analyze();
+
+    let expected = constructExpectedResult("text");
+
+    expect(CompareTagObject(expected, result)).toBe(true);
+})
+
+constructExpectedResult = (mode) =>{
+    let expected ={
+        keywords : {
+            "news" : "class",
+            "td" : "tagName",
+            "headlines" : "class",
+            "span" : "tagName",
+            "li" : "tagName"
+        },
+        mode : mode
+    };
+    return expected;
+}
 
 //TODO voir si on a deux fois la même balise dans la chaîne !!!!!!!
